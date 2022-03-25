ccac2bf30a59ab06f28deb8001b7e8df02284233
IChylewski <ireneuszchylewski@gmail.com>
2022-03-25 10:47:19 +0000

    LoginPage - WindowBarAdded

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.IconPacks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -42,6 +43,39 @@ namespace RugbyManagementSystem
         private void OnLogInClick(Object sender, RoutedEventArgs args)
         {
             MessageBox.Show(PasswordBox.Password.ToString());
+        }
+        private void Border_MouseDown(Object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+        private void ButtonMinimize_Click(Object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+        private void ButtonMaximize_Click(Object sender, MouseButtonEventArgs e)
+        {
+            PackIconMaterial maximizeIcon = sender as PackIconMaterial;
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+
+            maximizeIcon.Kind = PackIconMaterialKind.WindowRestore;
+            maximizeIcon.MouseDown -= ButtonMaximize_Click;
+            maximizeIcon.MouseDown += ButtonRestore_Click;
+        }
+        private void ButtonRestore_Click(Object sender, MouseButtonEventArgs e)
+        {
+            PackIconMaterial restoreIcon = sender as PackIconMaterial;
+            Application.Current.MainWindow.WindowState = WindowState.Normal;
+
+            restoreIcon.Kind = PackIconMaterialKind.WindowMaximize;
+            restoreIcon.MouseDown -= ButtonRestore_Click;
+            restoreIcon.MouseDown += ButtonMaximize_Click;
+        }
+        private void ButtonExit_Click(Object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
         }
     }
 }
